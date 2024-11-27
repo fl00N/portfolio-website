2b67977add1e470e80202f9fb4fbb2cf434b2e7d
Andrii Bondar <andybondar3@gmail.com>
2024-11-27 20:40:02 +0000

    fix

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,25 @@
-import Start from "../Start/Start"
+import { useEffect, useState } from 'react';
 import transition from "../Animation/transition";
 import ReactLenis from "@studio-freight/react-lenis";
 import Navbar from "../Navbar/Navbar";
-import Skills from "../Skills/Skills";
+import Start from "../Start/Start"
+import Skills from "../Skills/DesktopSkills/Skills";
+import MobileSkills from "../Skills/MobileSkills/MobileSkills";
 import Footer from "../Footer/Footer";
 
 const Main = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <ReactLenis root>
@@ -13,8 +27,9 @@ const Main = () => {
 
       <div className="main">
         <Start />
-        <Skills />
-        <Footer />
+        {!isMobile ? <Skills /> : <MobileSkills />}
+
+        {!isMobile && <Footer />}
       </div>
     </ReactLenis>
   )
---
+++ src/components/Skills/MobileSkills/MobileSkills.jsx
@@ -0,0 +1,39 @@
+import './MobileSkills.css';
+import ReactLenis from '@studio-freight/react-lenis'
+
+const MobileSkills = () => {
+
+    const backTitles = [
+        "Frontend Technologies",
+        'Frameworks and Libraries',
+        'State and animation',
+        "Additional skills",
+    ];
+
+    const backTexts = [
+        "HTML, CSS, Tailwind CSS, JavaScript, TypeScript",
+        'React.js, Next.js, Three.js',
+        'Zustand, Context API, GSAP, Framer Motion',
+        "Responsive Design, OOP, Git, REST API, Vercel, Gemini AI, Node.js, Express.js, MongoDB, Firebase",
+    ];
+
+    return (
+        <ReactLenis root>
+            <div className="container-mobile">
+                <div className='cards-mobile'>
+                    {[...Array(4)].map((_, index) => (
+                        <div
+                            key={index}
+                            className='card-mobile'
+                        >
+                            <h1>{backTitles[index]}</h1>
+                            <p>{backTexts[index]}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </ReactLenis>
+    );
+};
+
+export default MobileSkills;
