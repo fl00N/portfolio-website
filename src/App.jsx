7c2afbd93f49ecc43f8b18db02d611246bd0638b
Andrii Bondar <andybondar3@gmail.com>
2024-12-13 16:41:33 +0000

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import { Routes, Route, useLocation } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
-import Projects from './components/Projects/Projects';
-import Contacts from './components/Contacts/Contacts';
-import Main from './components/Main/Main';
-import './App.css';
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import Projects from "./components/Projects/Projects";
+import Contacts from "./components/Contacts/Contacts";
+import Main from "./components/Main/Main";
+import "./App.css";
 
 const App = () => {
-
   const location = useLocation();
 
   return (
     <>
-          <AnimatePresence mode='wait'>
-            <Routes location={location} key={location.pathname}>
-              <Route path='/' element={<Main />} />
-              <Route path='/projects' element={<Projects />} />
-              <Route path='/contacts' element={<Contacts />} />
-            </Routes>
-          </AnimatePresence>
-        </>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Main />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+      </AnimatePresence>
+    </>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
